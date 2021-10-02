a99b9faeaad2d3d57f442626d7c3b00febb4517d
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-10-02 17:07:15 -0300

    feat: finalizando rotas principais das categories ⭐

--- api/src/app/controllers/CategoryController.js
+++ api/src/app/controllers/CategoryController.js
@@ -1,13 +1,53 @@
+const CategoryRepository = require('../repositories/CategoryRepository');
+
 class CategoriesController {
   show() {}
 
-  index() {}
+  async index(request, response) {
+    const { orderBy } = request.query;
 
-  store() {}
+    const categories = await CategoryRepository.findAll(orderBy);
 
-  update() {}
+    response.json(categories);
+  }
 
-  delete() {}
+  async store(request, response) {
+    const { name } = request.body;
+
+    const category = await CategoryRepository.create({ name });
+
+    response.json(category);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoryRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).send({
+        error: 'pokemon not found',
+      });
+    }
+
+    if (!name) {
+      return response.status(400).send({
+        error: 'name is required',
+      });
+    }
+
+    const category = await CategoryRepository.update(id, { name });
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    await CategoryRepository.delete(id);
+
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoriesController();
--- api/src/app/repositories/CategoryRepository.js
+++ api/src/app/repositories/CategoryRepository.js
@@ -1,5 +1,55 @@
-class CategoryRepository {
+const db = require('../../database');
 
+class CategoryRepository {
+  async findById(id) {
+    const [row] = await db.query(`
+    SELECT * FROM categories
+    WHERE id = $1
+    `, [id]);
+
+    return row;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(`
+    INSERT INTO categories(name)
+    VALUES($1)
+    RETURNING *
+    `, [name]);
+
+    return row;
+  }
+
+  async update(id, { name }) {
+    const [row] = await db.query(`
+    UPDATE categories
+    SET name = $1
+    WHERE id = $2
+    RETURNING *
+    `, [name, id]);
+
+    return row;
+  }
+
+  async delete(id) {
+    const deleteOp = await db.query(`
+    DELETE FROM categories WHERE id = $1
+    `, [id]);
+
+    return deleteOp;
+  }
+
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+
+    const row = await db.query(`
+    SELECT * FROM categories ORDER BY name ${direction}
+    `);
+
+    return row;
+  }
+
+  findPokemonsByCategory() {}
 }
 
-export default new CategoryRepository();
+module.exports = new CategoryRepository();
