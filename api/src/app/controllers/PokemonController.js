03bc9ca7c2c49ae99eb46d47304db80ee4d5b307
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-10-02 13:55:02 -0300

    feat: finalizando rotas dos pokemons  🎮

--- api/src/app/controllers/PokemonController.js
+++ api/src/app/controllers/PokemonController.js
@@ -1,7 +1,13 @@
 const PokemonRepository = require('../repositories/PokemonRepository');
 
 class PokemonController {
-  show() {}
+  async show(request, response) {
+    const { id } = request.params;
+
+    const pokemon = await PokemonRepository.findById(id);
+
+    response.json(pokemon);
+  }
 
   async index(request, response) {
     const { orderBy } = request.query;
@@ -11,11 +17,64 @@ class PokemonController {
     response.json(pokemons);
   }
 
-  store() {}
+  async store(request, response) {
+    const {
+      name, url, description, category_id,
+    } = request.body;
 
-  update() {}
+    if (!name) {
+      response.status(400).json({
+        error: 'Name is required',
+      });
+    }
 
-  delete() {}
+    const pokemon = await PokemonRepository.create({
+      name, url, description, category_id,
+    });
+
+    response.json(pokemon);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, url, description, category_id,
+    } = request.body;
+
+    const pokemonExists = await PokemonRepository.findById(id);
+
+    if (!pokemonExists) {
+      return response.status(404).send({
+        error: 'pokemon not found',
+      });
+    }
+
+    if (!name) {
+      return response.status(400).send({
+        error: 'name is required',
+      });
+    }
+
+    if (!url) {
+      return response.status(400).send({
+        error: 'url is required',
+      });
+    }
+
+    const pokemon = await PokemonRepository.update(id, {
+      name, url, description, category_id,
+    });
+
+    response.json(pokemon);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await PokemonRepository.delete(id);
+
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new PokemonController();
--- api/src/app/repositories/PokemonRepository.js
+++ api/src/app/repositories/PokemonRepository.js
@@ -11,13 +11,45 @@ class PokemonRepository {
     return row;
   }
 
-  findById() {}
+  async findById(id) {
+    const [row] = await db.query(`
+    SELECT * FROM pokemons WHERE id = $1
+    `, [id]);
+    return row;
+  }
 
-  create() {}
+  async create({
+    name, url, description, category_id,
+  }) {
+    const [row] = await db.query(`
+      INSERT INTO pokemons(name, url, description, category_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+      `, [name, url, description, category_id]);
 
-  delete() {}
+    return row;
+  }
 
-  update() {}
+  async delete(id) {
+    const deleteOp = await db.query(`
+    DELETE FROM pokemons WHERE id = $1
+    `, [id]);
+
+    return deleteOp;
+  }
+
+  async update(id, {
+    name, url, description, category_id,
+  }) {
+    const [row] = await db.query(`
+    UPDATE pokemons
+    SET name = $1, url = $2, description = $3, category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `, [name, url, description, category_id, id]);
+
+    return row;
+  }
 }
 
 module.exports = new PokemonRepository();
